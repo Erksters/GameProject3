59f2db1c7de8d67fc37868d9b88b125fb6692801
Erick PC <erksters@gmail.com>
2021-03-02 16:12:59 -0600

    Added textures for skeleton figure. Left and right movements are functional and gravity is functional

--- GameProject3/Protagonist.cs
+++ GameProject3/Protagonist.cs
@@ -10,6 +10,8 @@ namespace GameProject3
 {
     class Protagonist
     {
+        public Game game;
+
         #region Animation
         /// <summary>
         /// Helps flip the animation in the Draw()
@@ -30,28 +32,37 @@ namespace GameProject3
         /// Helps animate the sprite
         /// will help loop the animation sprite back to frame 0
         /// </summary>
-        private int totalFrames;
+        private int totalFrames = 11;
 
         /// <summary>
         /// Helps animate the sprite
         /// Determines how quickly the draw() goes through frames
+        /// Smaller is faster
         /// </summary>
-        private double animationSpeed;
+        private double animationSpeed = 0.1;
 
         /// <summary>
         /// height of the animations sprite
         /// </summary>
-        private int height;
+        private int idleHeight = 32;
 
         /// <summary>
         /// width of the animations sprite
         /// </summary>
-        private int width;
+        private int idleWidth = 24;
+
+        private int movingWidth = 22;
 
         /// <summary>
         /// texture object to display in the Draw()
         /// </summary>
-        private Texture2D texture;
+        private Texture2D idleTexture;
+        
+        /// <summary>
+        /// texture object to display in the Draw()
+        /// </summary>
+        private Texture2D WalkingTexture;
+
         #endregion
 
         /// <summary>
@@ -105,7 +116,14 @@ namespace GameProject3
         /// <summary>
         /// Used for constant application of speed onto Position property
         /// </summary>
-        public Vector2 Velocity;
+        public Vector2 HorizontalVelocity = new Vector2(3, 0);
+
+        private Vector2 Velocity = new Vector2(0, 0);
+
+        /// <summary>
+        /// The value of gravity in this environment
+        /// </summary>
+        public Vector2 Gravity = new Vector2(0, 50);
 
         /// <summary>
         /// Determines where to draw the sprite
@@ -118,14 +136,21 @@ namespace GameProject3
         private int jumpingTimer = 1;
 
         /// <summary>
+        /// Helps determine if the player is on a platform
+        /// </summary>
+        public bool OnPlatform;
+
+        /// <summary>
         /// public constructor
         /// </summary>
-        public Protagonist(Vector2 initialPosition)
+        public Protagonist(Game game, Vector2 initialPosition)
         {
-            if(initialPosition == null)
+            this.game = game;
+
+            //if(initialPosition == null) { throw new ArgumentException(); }
             Position = initialPosition;
             initialPosition = Position;
-            RectangleBounds = new BoundingRectangle(initialPosition, width, height);
+            RectangleBounds = new BoundingRectangle(initialPosition, idleWidth, idleHeight);
         }
 
         /// <summary>
@@ -139,8 +164,9 @@ namespace GameProject3
         /// <param name="jumping">determines if we are actively holding the up key</param>
         /// <param name="attacking">determines if we are pressing the attack key</param>
         /// <param name="pressingShift">determines if we are pressing the shift key</param>
-        public void Update(GameTime gameTime, Movement direction, bool moving, bool flipTexture, bool jumping, bool attacking, bool pressingShift )
+        public void Update(GameTime gameTime, Movement direction, bool moving, bool flipTexture, bool jumping, bool attacking, bool pressingShift, bool onPlatform, bool stopLeftMovement, bool stopRightMovement , bool stopJump, bool stopFall , bool stopMoving)
         {
+
             //Update state of protagonist
             Direction = direction;
             Moving = moving;
@@ -148,8 +174,39 @@ namespace GameProject3
             Jumping = jumping;
             Attacking = attacking;
             Shift = pressingShift;
+            OnPlatform = onPlatform;
+
+            if (stopMoving)
+            {
+                return;
+            }
 
             //TODO: Get the protagonist to move correctly
+            switch (direction)
+            {
+                case Movement.Idle:
+                    break;
+                case Movement.Left:
+                    Position += -HorizontalVelocity * SpeedMultiplier;
+                    RectangleBounds.X += Position.X;
+                    RectangleBounds.Y += Position.Y;
+
+                    break;
+
+                case Movement.Right:
+                    Position += HorizontalVelocity * SpeedMultiplier;
+                    RectangleBounds.X += Position.X;
+                    RectangleBounds.Y += Position.Y;
+                    break;
+
+                case Movement.Up:
+                    break;
+                case Movement.Down:
+                    break;
+            }
+
+            applyGravity(gameTime);
+     
         }
 
         /// <summary>
@@ -159,6 +216,8 @@ namespace GameProject3
         /// <param name="spriteBatch"></param>
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
+            checkTextures();
+
             //Update animation timer
             animationTimer += gameTime.ElapsedGameTime.TotalSeconds;
 
@@ -167,21 +226,22 @@ namespace GameProject3
             {
                 animationTimer -= animationSpeed;
                 animationFrame++;
-                if (animationFrame > totalFrames)
+                if (animationFrame > 12)
+                {
+                    animationFrame = 0;
+                }
+                if (!Moving && animationFrame > 10)
                 {
                     animationFrame = 0;
                 }
             }
 
-            if (Flipped)
-            {
-                spriteEffect = SpriteEffects.FlipHorizontally;
-            }
-            
-            ///source = rectangle(Which frame to draw (x-coordinate), Which frame to draw (y-coordinate), width of frame, height of frame
-            var source = new Rectangle(animationFrame * width, 0, width, height);
+            handleSpriteEffect(Flipped);
 
-            spriteBatch.Draw(texture, Position, source, Color.White, 0f, new Vector2(150, 150), 1, spriteEffect, 0);
+            //source = rectangle(Which frame to draw (x-coordinate), Which frame to draw (y-coordinate), width of frame, height of frame
+            var source = new Rectangle(animationFrame * ((Moving) ? movingWidth : idleWidth), 0, ((Moving) ? movingWidth : idleWidth), idleHeight);
+
+            spriteBatch.Draw((Moving ? WalkingTexture : idleTexture), Position, source, Color.White, 0f, new Vector2(idleWidth, idleHeight), 1, spriteEffect, 0);
         }
 
         /// <summary>
@@ -190,7 +250,7 @@ namespace GameProject3
         public void ResetGame()
         {
             Position = initialPosition;
-            RectangleBounds = new BoundingRectangle(initialPosition, width, height);
+            RectangleBounds = new BoundingRectangle(initialPosition, idleWidth, idleHeight);
             Jumping = false;
             Moving = false;
             Shift = false;
@@ -199,12 +259,51 @@ namespace GameProject3
         }
 
         /// <summary>
+        /// Helper function for applying gravity onto our position attribute
+        /// Used in Update()
+        /// </summary>
+        /// <param name="gameTime"></param>
+        private void applyGravity(GameTime gameTime)
+        {
+            Velocity += Gravity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            Position += Velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+
+        /// <summary>
         /// Load the texture sprite for the animation
         /// </summary>
         /// <param name="content"></param>
         public void LoadContent(ContentManager content)
         {
-            texture = content.Load<Texture2D>("TODO");
+            idleTexture = content.Load<Texture2D>("SkeletonIdle");
+            WalkingTexture = content.Load<Texture2D>("Skeleton Walk");
+        }
+
+        /// <summary>
+        /// Helper function to determine if we need to flip the direction of the animation
+        /// Used in Draw()
+        /// </summary>
+        /// <param name="flipped"></param>
+        private void handleSpriteEffect(bool flipped)
+        {
+            if (Flipped)
+            {
+                spriteEffect = SpriteEffects.FlipHorizontally;
+            }
+            else
+            {
+                spriteEffect = SpriteEffects.None;
+            }
+        }
+
+        /// <summary>
+        /// Helper function to determine if the texture objects loaded correctly
+        /// Used in LoadContent()
+        /// </summary>
+        private void checkTextures()
+        {
+            if (idleTexture is null) throw new InvalidOperationException("Texture must be loaded to render");
+            if (WalkingTexture is null) throw new InvalidOperationException("Texture must be loaded to render");
         }
     }
 }
