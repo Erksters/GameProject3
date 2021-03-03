6c2b36037347f0aa3e793f187c391e76cbef45f5
Erick PC <erksters@gmail.com>
2021-03-03 12:54:10 -0600

    Imroved Idle vs Walk vs Attack relationship for Protagonist and IO mangager

---
+++ GameProject3/ProtagonistState.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject3
+{ 
+    /// <summary>
+    /// Helps Determine the players State.
+    /// </summary>
+    public enum ProtagonistState { Idle, Walking, Attacking}
+}
